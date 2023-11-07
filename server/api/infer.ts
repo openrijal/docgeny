30f20102dd8c29b6c72023388d52966f20ea9978
Dipesh Acharya <dipesh@awecode.com>
2023-11-07 22:50:02 +0545

    feat: add support for system and user prompt

--- server/api/infer.ts
+++ server/api/infer.ts
@@ -2,15 +2,22 @@ export default eventHandler(async (event) => {
   const ACCOUNT_ID = process.env.ACCOUNT_ID
   const TOKEN = process.env.CF_TOKEN
   const url = `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/ai/run/@cf/meta/llama-2-7b-chat-int8`
+
+  const messages = [
+    {
+      role: 'system',
+      content: 'You reply in only one sentence.',
+    },
+    { role: 'user', content: 'What is the origin of the phrase Hello, World' },
+  ]
+
   const response = $fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${TOKEN}`,
     },
-    body: {
-      prompt: 'Where is Nepal?',
-    },
+    body: { messages },
   })
   return response
 })
