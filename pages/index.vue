f6a127cdfa9366af178c63932e67a239ff1f45b1
Dipesh Acharya <dipesh@awecode.com>
2023-11-07 22:06:32 +0545

    feat: create form for cover letter generator

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,54 @@
 <template>
-  <div>Hi, I am index!</div>
+  <div class="mx-auto max-w-md space-y-6 mt-16">
+    <div class="space-y-2 text-center">
+      <h1 class="text-3xl font-bold">Cover Letter Generator</h1>
+      <p class="text-zinc-500 dark:text-zinc-400">
+        Enter your details to generate a personalized cover letter
+      </p>
+    </div>
+    <div class="space-y-4">
+      <div class="space-y-2">
+        <label
+          class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          for="name"
+          >Name</label
+        ><input
+          class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+          id="name"
+          placeholder="Enter your name"
+          required
+        />
+      </div>
+      <div class="space-y-2">
+        <label
+          class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          for="position"
+          >Position</label
+        ><input
+          class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+          id="position"
+          placeholder="Enter the position you're applying for"
+          required
+        />
+      </div>
+      <div class="space-y-2">
+        <label
+          class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          for="company"
+          >Company</label
+        ><input
+          class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+          id="company"
+          placeholder="Enter the company name"
+          required
+        />
+      </div>
+      <button
+        class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
+        type="submit"
+      >
+        Generate Cover Letter
+      </button>
+    </div>
+  </div>
 </template>
