df31873a8e158fb81dc6cb357ee3463b600e63b6
Dipesh Acharya <dipesh@awecode.com>
2023-11-07 23:09:53 +0545

    feat: build user prompt from form data

--- server/api/infer.ts
+++ server/api/infer.ts
@@ -3,6 +3,11 @@ export default eventHandler(async (event) => {
   const TOKEN = process.env.CF_TOKEN
   const url = `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/ai/run/@cf/meta/llama-2-7b-chat-int8`
 
+  const body = await readBody(event)
+  // @ts-ignore
+  const userPrompt = `Name: ${body.name}, Position:${body.position}, Company: ${body.company}`
+  // return userPrompt
+
   const messages = [
     {
       role: 'system',
@@ -11,8 +16,7 @@ export default eventHandler(async (event) => {
     },
     {
       role: 'user',
-      content:
-        'Name: Dipesh Acharya, Position: Nuxt Developer, Company: Nuxt Labs',
+      content: userPrompt,
     },
   ]
 
@@ -25,6 +29,7 @@ export default eventHandler(async (event) => {
     body: { messages },
   })
 
+  // @ts-ignore
   const response = inference.result.response
 
   return { response }
